3682299159067d60e65cc101c40befba2d01cc1f
Valery Semenenko <gearmobile@gmail.com>
2023-11-28 10:36:48 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface IItem {
+  title: string,
+  description: string
+}
+
+class Item implements IItem {
+  constructor(public title: string, public description: string) {
+    //
+  }
+}
+
 @Component({
   selector: 'f-root',
   standalone: true,
@@ -9,5 +20,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'for-syntax';
+  public readonly items: IItem[] = [
+    new Item('title 1', 'description 1'),
+    new Item('title 2', 'description 2'),
+    new Item('title 3', 'description 3'),
+  ];
 }

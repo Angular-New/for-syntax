6e0bc67c60344c8b9ad81a1eb34352e578f2bd94
Verda Muso <gearmobile@gmail.com>
2023-12-12 23:07:15 +0300

    add @switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ItemComponent } from './components/item/item.component';
 import { AppService } from './app.service';
 import { Observable } from 'rxjs';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { EColor } from './enums';
 
 @Component({
   selector: 'f-root',
@@ -15,9 +16,11 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  protected readonly EColor = EColor;
   private readonly appService: AppService = inject(AppService);
   public readonly items$: Observable<IItem[]> = this.appService.generateItems();
   public readonly maps$: Observable<Map<string, string>> = this.appService.generateMap();
+  public readonly color: EColor = EColor.Blue;
 
   // writing our own tracking function, if we really need to
   // but in most cases, the shorthand notation - track item.title - will be enough
---
+++ src/app/enums/color.ts
@@ -0,0 +1,5 @@
+export enum EColor {
+  Blue = 'blue',
+  Black = 'black',
+  Red = 'red'
+}
